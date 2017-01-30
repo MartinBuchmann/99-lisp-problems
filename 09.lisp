ad9a07be58bf0d24bc81acc0a34bf1661d46d6ad
Martin Buchmann <Martin.Buchmann@googlemail.com>
2017-01-30 20:45:09 +0100

    Revisited `pack` to implement a recursive version.

--- 09.lisp
+++ 09.lisp
@@ -6,26 +6,14 @@
 ;;; * (pack '(a a a a b c c a a d e e e e))
 ;;; ((A A A A) (B) (C C) (A A) (D) (E E E E))
 (defun pack (original-list)
-  "Packs consecutive duplicates of list elements into sublists."
-  (unless (or (null original-list) (not (listp original-list)))
-    (cons (pack-aux-1 original-list) (pack (pack-aux-2 original-list)))))
-
-(defun pack-aux-1 (original-list)
-  "Creates a new list from atoms of the given list `original-list' until an atom differs
-  from the first one."
-  (cond ((null original-list) nil)
-	((null (rest original-list)) original-list)
-	((eql (first original-list) (second original-list))
-	 (cons (first original-list) (pack-aux-1 (rest original-list))))
-	(t (list (first original-list)))))
-
-(defun pack-aux-2 (original-list)
-  "Ignores the first element(s) of a list until an atom differs from the first one."
-  (cond ((null original-list) nil)
-	((null (rest original-list)) nil)
-	((eql (first original-list) (second original-list))
-	 (pack-aux-2 (rest original-list)))
-	(t (rest original-list))))
+  "Packs/groups consecutive duplicates of list elements into sublists."
+  (labels ((group-elements (element group list)
+             (cond
+               ((null list) (list (cons element group)))
+               ((eql element (first list)) (group-elements element (cons element group) (rest list)))
+               (t (cons (cons element group) (group-elements (first list) '() (rest list)))))))
+    (unless (null original-list)
+      (group-elements (first original-list) '() (rest original-list)))))
 
 (define-test test-pack
   (assert-nil (pack '()))
